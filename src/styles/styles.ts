7e3e9634900801436333c33879dafc850e6d6608
Guilherme Ramos <guilherme.ramos@matchboxbrasil.com>
2022-07-12 20:38:52 -0300

    feat: creating project and adding chakra-ui

---
+++ src/styles/styles.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    purple: {
+      200: '#a578e3',
+    },
+    green: {
+      200: '#c2f776',
+    },
+  },
+});
